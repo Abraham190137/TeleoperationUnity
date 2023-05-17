1b03ee075916fd2d6e44e14777093ca975ef9499
Abraham George <aigeorge@andrew.cmu.edu>
2023-05-17 18:00:06 -0400

    fixed panda-gym controll

--- Robot Control - Python/PandaGym Scripts/server_env.py
+++ Robot Control - Python/PandaGym Scripts/server_env.py
@@ -44,9 +44,9 @@ def generate_hand_message(hand_position, hand_rotation, finger_width):
 sock = U.UdpComms(udpIP=THIS_IP, sendIP = OCULUS_IP, portTX=8000, portRX=8001, enableRX=True, suppressWarnings=False)
 
 if OLD_PANDAGYM:
-    env = PandaStackEnv(render = True) # task enviroment
+    env = PandaStackEnv(render = True, control_type="joints") # task enviroment
 else:
-    env = PandaStackEnv(render_mode = "human") # task enviroment
+    env = PandaStackEnv(render_mode = "human", control_type="joints") # task enviroment
 
 env.reset()
 position = env.robot.get_ee_position()
@@ -73,6 +73,9 @@ send_update_time = time.time() - DELAY
 sim_update_time = time.time() - ENV_STEP_PERIOD
 
 rotation = np.array([1, 0, 0, 0])
+goal_joints = np.empty(9)
+for i in range(9):
+    goal_joints[i] = env.robot.get_joint_angle(i)
 
 while(True):
     if time.time() > send_update_time + DELAY:
@@ -108,34 +111,40 @@ while(True):
             object_index = 0
 
             position_message, rotation_message = hand_pose.split('\t')
-            position = np.array(position_message[1:-1].split(', ')).astype(np.float)
+            position = np.array(position_message[1:-1].split(', ')).astype(np.float64)
             position = np.array([position[2], -position[0], position[1]])
-            rotation = np.array(rotation_message[1:-1].split(', ')).astype(np.float)
+            rotation = np.array(rotation_message[1:-1].split(', ')).astype(np.float64)
             rotation = np.array([-rotation[2], rotation[0], -rotation[1], rotation[3]])
-            if gripper_message != "":
+            if gripper_message == "":
                 continue
 
-            gripper = np.array(gripper_message.split('\t')).astype(np.float)
-            gripper = gripper[0]
+            gripper = np.array(gripper_message.split('\t')).astype(np.float64)[0]
+            print(gripper)
 
             # Update the position of the simulated panda robot acording to the recieved message
-            goal_joints = np.zeros(9)
             goal_joints[:7] = env.robot.inverse_kinematics(11, position, rotation)[:7]    
             
-            goal_joints[7:] = gripper/2
-            env.robot.set_joint_angles(goal_joints)
+            goal_joints[7:] = gripper*2
+            # env.robot.set_joint_angles(goal_joints)
 
     # Update the panda-gym simulation
     if time.time() > sim_update_time + ENV_STEP_PERIOD: # each env step takes 40ms, so step env every 0.04 seconds
         sim_update_time += ENV_STEP_PERIOD
-        error = np.zeros(4)
-        error[:3] = position - env.robot.get_ee_position()
-        error[3] = gripper - env.robot.get_fingers_width()
+        joint_angles = np.empty(9)
+        for i in range(7):
+            joint_angles[i] = env.robot.get_joint_angle(i)
+        
+        joint_angles[7:] = env.robot.get_fingers_width()
+            
+
+        error = goal_joints - joint_angles
 
         if OLD_PANDAGYM:
-            obs, reward, done, info = env.step(10*np.array([1, 1, 1, 0.07])*error)
+            obs, reward, done, info = env.step(10*error[:8])
         else:
-            obs, reward, done, _, info = env.step(10*np.array([1, 1, 1, 0.07])*error)
+            obs, reward, done, _, info = env.step(10*error[:8])
+
+        print(joint_angles)
 
         # Reset the enviroment if the task is completed
         if RESET_AT_TASK_COMPLETION:
